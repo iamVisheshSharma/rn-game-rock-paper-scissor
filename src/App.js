f6d767d87ff0eaa5de2469f44df93d1b21cb26ee
= <=>
2023-01-02 22:01:12 +0530

    Add the project structure

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/**
+ * This is gaming project and creates by Vishesh and Bhuvi 
+ * We're creating this project just for fun 
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+      <SafeAreaView>
+        <View>
+          <Text>Vishesh in the house lets rock the party.</Text>
+          <Text>This code is just for the testing.</Text>
+        </View>
+      </SafeAreaView>
+  );
+};
+
+export default App;
