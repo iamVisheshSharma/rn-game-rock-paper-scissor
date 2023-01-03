3cd3f0aede493c7dd5fed38dd2126ec1595b6891
= <=>
2023-01-03 22:19:26 +0530

    Create the splash screen animation

--- src/screens/splashStack/splashScreen/index.js
+++ src/screens/splashStack/splashScreen/index.js
@@ -1,46 +1,118 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  Animated,
-  View,
-  TouchableOpacity,
-  Text,
-} from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, SafeAreaView, Text, TouchableOpacity, Animated } from 'react-native';
+import { WIDTH, HEIGHT } from '../../../utils/Dimension';
 
-export default function SplashScreen() {
-  const value = React.useState(new Animated.ValueXY({x: 0, y: 0}))[0];
-
-  function move() {
-    Animated.timing(value, {
-      toValue: {x: 0, y: -300},
-      duration: 250,
-      useNativeDriver: false,
-    }).start();
+export default class Bounce extends Component {
+  state = {
+    animation: new Animated.Value(20),
+    ballWidth: new Animated.Value(100),
+    ballFadeOut: new Animated.Value(1),
+    logoFadeIn: new Animated.Value(0),
+    isLogoVisible: false,
+    isTextVisible: false
   }
 
-  React.useEffect(() => {
-    move()
-  })
 
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <View
-        style={{
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          height: '100%',
-        }}>
-        <Animated.View style={value.getLayout()}>
-          <View
-            style={{
-              borderRadius: 100 / 2,
-              height: 100,
-              width: 100,
-              backgroundColor: 'red',
-            }}
-          />
-        </Animated.View>
-      </View>
-    </SafeAreaView>
-  );
+  componentDidMount() {
+    this.handlePress();
+  }
+
+  handlePress = () => {
+
+    Animated.spring(this.state.animation, {
+      toValue: HEIGHT("40%"),
+      friction: 0.5,
+      tension: 1,
+      useNativeDriver: false
+    }).start(() => {
+      Animated.timing(this.state.ballWidth, {
+        duration: 1000,
+        toValue: WIDTH("90%"),
+        useNativeDriver: false,
+      }).start((finished) => {
+        if (finished.finished) this.setState({ isTextVisible: true });
+        setTimeout(() => {
+          Animated.timing(this.state.ballFadeOut, {
+            duration: 4000,
+            toValue: 0,
+            useNativeDriver: false,
+          }).start((finished) => {
+            if (finished.finished) this.setState({ isLogoVisible: true });
+            Animated.timing(this.state.ballFadeOut, {
+              duration: 4000,
+              toValue: 0,
+              useNativeDriver: false,
+            }).start();
+            Animated.timing(this.state.logoFadeIn, {
+              duration: 2000,
+              toValue: 1,
+              useNativeDriver: false,
+            }).start();
+          });
+        }, 1000);
+      });
+    });
+    // Animated.sequence([
+    //   ,
+    // ]).start((finished) => {
+    //   if (finished.finished) this.setState({ isTextVisible: true });
+    //   Animated.timing(this.state.ballFadeOut, {
+    //     duration: 4000,
+    //     toValue: 0,
+    //     useNativeDriver: false,
+    //   }).start()
+    // });
+  }
+
+
+  render() {
+    return (
+      <>
+        <SafeAreaView>
+          <Animated.View style={{
+            width: this.state.ballWidth,
+            height: 100,
+            borderRadius: 50,
+            backgroundColor: "tomato",
+            alignSelf: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            opacity: this.state.ballFadeOut,
+            transform: [{
+              translateY: this.state.animation,
+              // translateX: this.state.ballWidth
+              // scaleX: this.state.ballWidth
+            }]
+          }}>
+            {this.state.isTextVisible ? (
+              <Text>Hey Hi, How are you.</Text>
+            ) : null}
+          </Animated.View>
+          {/* {this.state.} */}
+          <Animated.View style={{opacity: this.state.logoFadeIn, height: 150, width: 300, borderWidth: 3, borderRadius: 10,alignSelf: "center",backgroundColor:"yellow",justifyContent:"center",alignItems:"center" }}>
+              <Text style={{fontSize:20,fontWeight:"bold"}}>RN-Game</Text>
+          </Animated.View>
+        </SafeAreaView>
+      </>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  ball: {
+
+  },
+  button: {
+    width: 150,
+    height: 70,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#fc5c65",
+    marginVertical: 50,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+});
--- src/utils/Dimension.js
+++ src/utils/Dimension.js
@@ -1,4 +1,24 @@
-import { Dimensions } from "react-native";
+import { Dimensions, PixelRatio } from "react-native";
+const { width, height } = Dimensions.get("window");
 
-export const windowWidth = Dimensions.get('window').width;
-export const windowHeight = Dimensions.get('window').height;
+
+/**
+ * @param {number} number 
+ * @returns actual width according to the device screen width. 
+ */
+const WIDTH = (number) => {
+    let givenWidth = typeof number === 'number' ? number : parseFloat(number);
+    return PixelRatio.roundToNearestPixel((width * givenWidth) / 100);
+}
+
+
+/**
+ * @param {number} number 
+ * @returns actual height according to the device screen height.
+ */
+const HEIGHT = (number) => {
+    let givenHeight = typeof number === "number" ? number : parseFloat(number);
+    return PixelRatio.roundToNearestPixel((height * givenHeight) / 100);
+}
+
+export { WIDTH, HEIGHT };
